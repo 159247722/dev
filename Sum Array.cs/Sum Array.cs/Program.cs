c3da4d43837db126428b94fc196e4bd3340182a0
159247722 <Anirudhbansal298@gmail.com>
2023-10-05 11:56:43 +0530

    first

---
+++ Sum Array.cs/Sum Array.cs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    { 
+
+        int sum = 0;
+        Console.WriteLine("Sum of the array elements");
+        for (int i = 0; i< 5; i++)
+        {
+
+            Console.WriteLine("Sum", arr[i]);
+        }
+
+       Console.WriteLine("Sum of array elements:");
+        for (int i = 0; i < 5; i++)
+        {
+            sum = sum + arr[i];
+        } 
+        Console.WriteLine("Sum",arr[i] );
+
+
+
+        Console.WriteLine("Sum", sum);
+        return 0;
+    }
+}
